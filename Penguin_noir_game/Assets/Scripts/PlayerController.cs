65bc3d953792a6207c1685cf51d42baceb22d5c9
Gavin Race <gavin.race16@gmail.com>
2014-04-07 16:26:31 -0700

    Added beginnings of script

---
+++ Penguin_noir_game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject cam;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(cam.transform.position, transform.position - cam.transform.position, 10));
+			Debug.DrawRay(cam.transform.position, transform.position - cam.transform.position);
+		}
+	
+	}
+}
