67574dba5e9d26d39e9dcabfc3d5abbef8f5ce62
Garrett Priddy <Kaboomm242@gmail.com>
2014-04-10 11:51:22 -0700

    Spotlights and Camera basics

--- Penguin_noir_game/Assets/Scripts/Camera_movement.cs
+++ Penguin_noir_game/Assets/Scripts/Camera_movement.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class Camera_movement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+	public Transform Look_pos;
+	public float posX;
+	public float posZ;
+	public float pos_up;
 	void Update () {
-	
+		//Lerping sounds better
+		Camera.main.gameObject.transform.position = new Vector3 (Look_pos.position.x + posX, pos_up, Look_pos.position.z + posZ);
+	}
+	void ScreenShake(){
+
 	}
 }
