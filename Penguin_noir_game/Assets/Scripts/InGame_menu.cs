6dcd32a1c96510a657b274a2b6a4f1ade8aeaecc
Garrett Priddy <Kaboomm242@gmail.com>
2014-04-10 17:35:35 -0700

    Gui Menu Tweaks

---
+++ Penguin_noir_game/Assets/Scripts/InGame_menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGame_menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Quit()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
