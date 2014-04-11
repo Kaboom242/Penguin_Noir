7f9ff558e8f42e36447d6bff5ac1bde53769bc48
Garrett Priddy <Kaboomm242@gmail.com>
2014-04-10 18:09:11 -0700

    Yeah I am working on Important stuff here... Screen Shake!

--- Penguin_noir_game/Assets/Scripts/Camera_movement.cs
+++ Penguin_noir_game/Assets/Scripts/Camera_movement.cs
@@ -6,11 +6,36 @@ public class Camera_movement : MonoBehaviour {
 	public float posX;
 	public float posZ;
 	public float pos_up;
-	void Update () {
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay;
+	float shake_intensity;
+	public float ShakeIT = .04f;
+	
+	void OnGUI (){
+		if (GUI.Button (new Rect (100,40,80,20), "Shake")){
+			Shake ();
+		}
+	}
+	
+	void Update (){
 		//Lerping sounds better
 		Camera.main.gameObject.transform.position = new Vector3 (Look_pos.position.x + posX, pos_up, Look_pos.position.z + posZ);
+		if (shake_intensity > 0){
+			transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+			transform.rotation = new Quaternion(
+				originRotation.x + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.y + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.z + Random.Range (-shake_intensity,shake_intensity) * .2f,
+				originRotation.w + Random.Range (-shake_intensity,shake_intensity) * .2f);
+			shake_intensity -= shake_decay;
+		}
 	}
-	void ScreenShake(){
-
+	
+	void Shake(){
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		shake_decay = 0.002f;
+		shake_intensity = ShakeIT;
 	}
 }
