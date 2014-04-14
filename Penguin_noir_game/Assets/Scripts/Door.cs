113e0aad69815798059af6c011a2807cefac925c
Eunice Chang <omgunis@gmail.com>
2014-04-14 14:39:15 -0700

    Revolving door and prefabs folder made

---
+++ Penguin_noir_game/Assets/Scripts/Door.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		iTweenEvent.GetEvent (gameObject, "DoorRot").Play();
+	}	
+}
