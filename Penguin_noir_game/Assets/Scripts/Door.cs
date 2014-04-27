bbcfc5e19e8c4a80907db5d85550a021d51ae900
Gavin Race <gavin.race16@gmail.com>
2014-04-27 00:53:27 -0700

    Added some BASIC random wandering NPC Ai. Super random, kind of makes them look insane. I'll refine it more later.

--- Penguin_noir_game/Assets/Scripts/Door.cs
+++ Penguin_noir_game/Assets/Scripts/Door.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
-	void OnTriggerEnter (Collider other) {
+	void OnTriggerEnter (Collider other) 
+	{
 		iTweenEvent.GetEvent (gameObject, "DoorRot").Play();
-	}	
+		
+	}
+	
 }
---
+++ Penguin_noir_game/Assets/Scripts/WanderAi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderAi : MonoBehaviour {
+
+	public float xRangeMax;
+	public float xRangeMin;
+	public float yRangeMax;
+	public float yRangeMin;
+	public float newSpotDistance;
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () 
+	{
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		agent.SetDestination (new Vector3(Random.Range(xRangeMin,xRangeMax),transform.position.y,Random.Range(yRangeMin,yRangeMax)));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (agent.remainingDistance < newSpotDistance)
+		{
+			SetNewDestination();
+		}
+	}
+	
+	void SetNewDestination ()
+	{
+		
+		agent.SetDestination (new Vector3(Random.Range(xRangeMin,xRangeMax),transform.position.y,Random.Range(yRangeMin,yRangeMax)));
+		Debug.DrawRay(agent.destination, Vector3.up, Color.red, 10, true);
+	}
+}
