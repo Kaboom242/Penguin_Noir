dc2f6a2dbcc5d40cab0063000d0d729512519b02
Gavin Race <gavin.race16@gmail.com>
2014-04-27 15:25:13 -0700

    Refined it. Ai now pauses a bit before wandering to new destination. More exposed variables to adjust.

--- Penguin_noir_game/Assets/Scripts/WanderAi.cs
+++ Penguin_noir_game/Assets/Scripts/WanderAi.cs
@@ -8,6 +8,9 @@ public class WanderAi : MonoBehaviour {
 	public float yRangeMax;
 	public float yRangeMin;
 	public float newSpotDistance;
+	public bool atDestination = false;
+	public float pauseTime;
+	private float waitTime;
 	private NavMeshAgent agent;
 	// Use this for initialization
 	void Start () 
@@ -20,16 +23,23 @@ public class WanderAi : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (agent.remainingDistance < newSpotDistance)
+		if (atDestination == false && agent.remainingDistance < newSpotDistance)
 		{
-			SetNewDestination();
+			waitTime = Random.Range(1.5f,3f);
+			Invoke("SetNewDestination",waitTime);
+			Debug.Log("pausing");
+			atDestination = true;
+			
+			
 		}
 	}
 	
 	void SetNewDestination ()
 	{
-		
 		agent.SetDestination (new Vector3(Random.Range(xRangeMin,xRangeMax),transform.position.y,Random.Range(yRangeMin,yRangeMax)));
 		Debug.DrawRay(agent.destination, Vector3.up, Color.red, 10, true);
+		atDestination = false;
 	}
+	
+	
 }
