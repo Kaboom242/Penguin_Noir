345208279358785e70a2bbe6d51f1ea820f12ce1
Biophision <hemitherrin@yahoo.com>
2014-06-09 14:44:14 -0700

    Updated "PlayerController" Script to include instantiation of "Click_PS" Particle System.

---
+++ Penguin_noir_game/Assets/Scripts/MouseClickBurst.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickBurst : MonoBehaviour
+{
+	public PlayerController PC;
+	public ParticleSystem ParticleInstance;
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			ParticleSystem clone;
+			clone = Instantiate(ParticleInstance, transform.position, transform.rotation) as ParticleSystem;
+		}
+	}
+}
